60e483689ece52a17c1df141d1036ff8b8c61dd5
Jacob Brunson <jacob.brunson@vivint.com>
2026-02-25 13:07:53 -0700

    Add function to hash a string

--- include/stinger/utils/hash.hpp
+++ include/stinger/utils/hash.hpp
@@ -8,6 +8,15 @@ namespace stinger {
 namespace utils {
 
 /**
+ * Compute a fast, non-cryptographic hash of a single string.
+ * Uses FNV-1a algorithm for speed and good distribution.
+ *
+ * @param str String to hash
+ * @return 64-bit hash value suitable for use as a map key
+ */
+uint64_t hashString(const std::string& str);
+
+/**
  * Compute a fast, non-cryptographic hash of a vector of strings.
  * Uses FNV-1a algorithm for speed and good distribution.
  *
--- src/hash.cpp
+++ src/hash.cpp
@@ -3,6 +3,18 @@
 namespace stinger {
 namespace utils {
 
+uint64_t hashString(const std::string& str) {
+    constexpr uint64_t FNV_OFFSET_BASIS = 14695981039346656037ULL;
+    constexpr uint64_t FNV_PRIME = 1099511628211ULL;
+
+    uint64_t hash = FNV_OFFSET_BASIS;
+    for (unsigned char c : str) {
+        hash ^= static_cast<uint64_t>(c);
+        hash *= FNV_PRIME;
+    }
+    return hash;
+}
+
 uint64_t hashStrings(const std::vector<std::string>& strings) {
     // FNV-1a hash constants for 64-bit
     constexpr uint64_t FNV_OFFSET_BASIS = 14695981039346656037ULL;
